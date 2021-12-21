c7193ebbcfd623344c3703deb14b72287d21a962
HerrNamenlos123 <herrnamenlos123@gmail.com>
2021-12-21 22:40:30 +0100

    Updated binaries

--- include/allegro5/allegro5/internal/aintern_image_cfg.h
+++ include/allegro5/allegro5/internal/aintern_image_cfg.h
@@ -5,11 +5,11 @@
 /* #undef ALLEGRO_CFG_IIO_HAVE_GDIPLUS */
 /* #undef ALLEGRO_CFG_IIO_HAVE_GDIPLUS_LOWERCASE_H */
 /* #undef ALLEGRO_CFG_IIO_HAVE_ANDROID */
-/* #undef ALLEGRO_CFG_IIO_HAVE_PNG */
+#define ALLEGRO_CFG_IIO_HAVE_PNG
 /* #undef ALLEGRO_CFG_IIO_HAVE_JPG */
 /* #undef ALLEGRO_CFG_IIO_HAVE_WEBP */
 
 /* which formats are supported and wanted? */
-/* #undef ALLEGRO_CFG_IIO_SUPPORT_PNG */
+#define ALLEGRO_CFG_IIO_SUPPORT_PNG
 /* #undef ALLEGRO_CFG_IIO_SUPPORT_JPG */
 /* #undef ALLEGRO_CFG_IIO_SUPPORT_WEBP */
